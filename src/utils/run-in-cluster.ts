2687bb7eb7a7bebe0da761b5f8fa2066c7ffc54c
Aleksandr Schemelev <prophreaker@gmail.com>
2023-07-15 14:35:51 +0300

    feat: update runInCluster function, restart if worker died, leave one core for master process

--- src/utils/run-in-cluster.ts
+++ src/utils/run-in-cluster.ts
@@ -2,11 +2,17 @@ import cluster from 'cluster';
 import os      from 'os';
 
 export function runInCluster(bootstrap: () => Promise<void>) {
-  const coresNum = os.cpus().length;
+  const coresNum = os.cpus().length - 1;
 
   if (cluster.isPrimary) {
     for (let i = 0; i < coresNum; ++i) {
-      cluster.fork();
+      const worker = cluster.fork();
+
+      worker.on('exit', () => {
+        console.log(`worker ${worker.process.pid} died`);
+        console.log(`worker ${worker.process.pid} restarting...`);
+        cluster.fork();
+      });
     }
   } else {
     bootstrap();
